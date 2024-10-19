73d38ef410c8049b9171d2aa3f91fad929c899dc
José Fabián Beltrán Meza <joshefbeltran@gmail.com>
2024-10-19 17:10:39 -0500

    POO library done

--- CursoBasico/library.py
+++ CursoBasico/library.py
@@ -34,3 +34,59 @@ class User:
             self.borrowed_books.remove(book)
         else:
             print(f"El libro {book.title} No esta en la lista de prestados")
+
+
+class Library:
+    def __init__(self) -> None:
+        self.books = []
+        self.users = []
+    
+    def add_book(self, book):
+        self.books.append(book)
+        print(f"El libro {book.title} ha sido agregado.")
+    
+    def register_user(self, user):
+        self.users.append(user)
+        print(f"El usuarion {user.name} ha sido registrado.")
+    
+    def show_available_books(self):
+        print(f"Libros disponibles: ")
+        for book in self.books:
+            if book.availabel:
+                print(f"\t - El libro {book.title} por {book.author}")
+    
+try: 
+    # Creando books
+    book1 = Book("Python a fondo", "Oscar Ramirez")
+    book2 = Book("gRPC Go for Profesionals", "Clement Jean")
+    book3 = Book("Microservices with Go", "Alexander Shuiskov")
+    book4 = Book("Patrones de Diseño", "Alecxander Shvets")
+    book5 = Book("Pro Go", "Adam Freeman")
+    #Creando el usuario
+    user1 = User("Fabian Beltran", 1 )
+    #Crear la biblioteca
+    library1 = Library()
+    #Agregando libros
+    library1.add_book(book1)
+    library1.add_book(book2)
+    library1.add_book(book3)
+    library1.add_book(book4)
+    library1.add_book(book5)
+    library1.register_user(user1)
+
+    #Mostrar libro
+    library1.show_available_books()
+
+    #Prestar libro.
+    user1.borrow_book(book2)
+    user1.borrow_book(book3)
+    user1.borrow_book(book4)
+    #Retornar libro
+    user1.return_book(book2)
+
+    library1.show_available_books()
+    
+
+except Exception as e:
+    print(f"Woops!. Hubo un error: ", e)
+    
