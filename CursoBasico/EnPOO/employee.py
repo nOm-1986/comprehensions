e6293c3b50498ab111cdacb7e569faec2e7203e6
José Fabián Beltrán Meza <joshefbeltran@gmail.com>
2025-01-03 18:40:22 -0500

    Working with args and kwarg

--- CursoBasico/EnPOO/employee.py
+++ CursoBasico/EnPOO/employee.py
@@ -5,4 +5,10 @@ class Employee:
         self.details = kwargs
 
     def show_employee(self):
-        print(f)
+        print(f"Employee: {self.name}")
+        print(f"Skills: {self.skills}")
+        print("Skills:", self.skills )
+        print('Details', self.details)
+
+employee = Employee('Jose', 'Js', 'Python', 'PHP', 'CSS', 'Golang', 'ReactNative', age=30, city='Bogotá')
+employee.show_employee()
