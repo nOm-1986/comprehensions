14e722821d84b12ae424c090af3e6040747c2210
José Fabián Beltrán Meza <joshefbeltran@gmail.com>
2025-01-11 22:24:18 -0500

    Pedidos asincronos

--- CursoBasico/ConcurrenciaParalelismo/pedidos_online.py
+++ CursoBasico/ConcurrenciaParalelismo/pedidos_online.py
@@ -1,3 +1,4 @@
+import multiprocessing.pool
 import time, random, multiprocessing, asyncio
 
 #Función asyncrona para verificar inventario.
@@ -31,5 +32,35 @@ def calculate_total(items):
 async def process_order(order_id, items):
     print(f'Iniciando el procesamiento de la orden {order_id} ....')
     # Verificar inventario para cada artículo
-    inventory_check = [check_inventory(item['name']) for item in items]
-    inventory_results = await asyncio.gather(*inventory_check)
+    inventory_checks = [check_inventory(item['name']) for item in items]
+    inventory_results = await asyncio.gather(*inventory_checks)
+
+    if not all(inventory_results):
+        print(f'Orden {order_id} was canceled: This product is not available."')
+
+    with multiprocessing.Pool() as pool:
+        total = pool.apply(calculate_total, (items,))
+    
+    # Procesar el pago asíncronicamente.
+    payment_result = await process_payment(order_id)
+
+    if payment_result:
+        print(f'Orden {order_id} completed. Total: {total}')
+    else:
+        print(f'It was an error processing payment {order_id}')
+
+
+async def main():
+    orders = [
+        {'order_id': 1, 'items': [{'name': 'Laptop', 'price': 1000}, {'name': 'Mouse', 'price': 50}]},
+        {'order_id': 2, 'items': [{'name': 'Teclado', 'price': 80}, {'name': 'Monitor', 'price': 300}]},
+        {'order_id': 3, 'items': [{'name': 'Smartphone', 'price': 700}, {'name': 'Funda', 'price': 20}]}
+    ]
+
+    #Procesar múltiples órdenes concurrentemente
+    tasks = [process_order(order['order_id'], order['items']) for order in orders]
+    await asyncio.gather(*tasks)
+
+#Creamos el event loop
+if __name__ == '__main__':
+    asyncio.run(main())
